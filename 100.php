7d600715d3cd5e2de75c7d90e43d600be084eecd
1 comiti <k00279922@gmail.com>
2019-07-19 22:06:39 +0400

    pirveli komiti

---
+++ 100.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+   <head>
+     <title> PHP CRUD</title>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+          <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<link rel="stylesheet" 
+    </head> 
+    <body> 
+    <form action="" method="POST">
+    <div class="form-group">
+    <label>Name</label>
+       <input type="text" name="name" value="Enter your name">
+       </div>
+       <div class="form-group">
+         <label>Location</label>
+         <input type="text" name="location" value="Enter your location">
+        <button type="submit" name="save">Save</button>
+    </form>
+</body>
